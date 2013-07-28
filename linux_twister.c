7115a7efd1b92b1677da08081cd333e3de303cf9
swgillespie <sean.william.g@gmail.com>
2013-07-27 19:11:00 -0700

    Switch to Linux style spacing

--- linux_twister.c
+++ linux_twister.c
@@ -70,138 +70,138 @@ static struct cdev char_device;
 static struct class *device_class;
 
 static void mersenne_init(int seed) {
-  int i;
-  index = 0;
-  state[0] = seed;
-  for (i = 1; i < MERSENNE_STATE_MATRIX_SIZE; i++) {
-    state[i] = 0xFFFFFFFF & (0x6C078965 * (state[i - 1] ^ (state[i - 1] >> 30)) + i);
-  }
+    int i;
+    index = 0;
+    state[0] = seed;
+    for (i = 1; i < MERSENNE_STATE_MATRIX_SIZE; i++) {
+	state[i] = 0xFFFFFFFF & (0x6C078965 * (state[i - 1] ^ (state[i - 1] >> 30)) + i);
+    }
 }
 
 static int mersenne_random_int(void) {
-  int out_num;
-  if (index == 0) {
-    mersenne_generate();
-  }
-  out_num = state[index];
-  out_num ^= (out_num >> 11);
-  out_num ^= (out_num << 7) & 0x9D2C5680;
-  out_num ^= (out_num << 15) & 0xEFC60000;
-  out_num ^= (out_num >> 18);
-  index = (index + 1) % MERSENNE_STATE_MATRIX_SIZE;
-  return out_num;
+    int out_num;
+    if (index == 0) {
+      mersenne_generate();
+    }
+    out_num = state[index];
+    out_num ^= (out_num >> 11);
+    out_num ^= (out_num << 7) & 0x9D2C5680;
+    out_num ^= (out_num << 15) & 0xEFC60000;
+    out_num ^= (out_num >> 18);
+    index = (index + 1) % MERSENNE_STATE_MATRIX_SIZE;
+    return out_num;
 }
 
 static void mersenne_generate(void) {
-  int i, y;
-  for (i = 0; i < MERSENNE_STATE_MATRIX_SIZE - 1; i++) {
-    y = (state[i] & 0x80000000) + (state[(i + 1) % MERSENNE_STATE_MATRIX_SIZE] & 0x7FFFFFFF);
-    state[i] = state[(i + 397) % MERSENNE_STATE_MATRIX_SIZE] ^ (y >> 1);
-    if ((y % 2) != 0) {
-      state[i] ^= 0x9908B0DF;
+    int i, y;
+    for (i = 0; i < MERSENNE_STATE_MATRIX_SIZE - 1; i++) {
+	y = (state[i] & 0x80000000) + (state[(i + 1) % MERSENNE_STATE_MATRIX_SIZE] & 0x7FFFFFFF);
+	state[i] = state[(i + 397) % MERSENNE_STATE_MATRIX_SIZE] ^ (y >> 1);
+	if ((y % 2) != 0) {
+	    state[i] ^= 0x9908B0DF;
+	}
     }
-  }
 }
 
 static int mersenne_fill_buf(char __user *buf, size_t size) {
-  int rand_num;
-  int err;
-  int buf_offset = 0;
-  if (size <= 0) {
-    return -EINVAL;
-  }
-  while(size / 4 > 0) {
-    rand_num = mersenne_random_int();
-    err = copy_to_user(buf + buf_offset, &rand_num, 4);
-    if (err != 0) {
-      return -EFAULT;
+    int rand_num;
+    int err;
+    int buf_offset = 0;
+    if (size <= 0) {
+	return -EINVAL;
     }
-    size -= 4;
-    buf_offset += 4;
-  }
-  // size is in the range 0 to 3
-  if (size != 0) {
-    rand_num = mersenne_random_int();
-    err = copy_to_user(buf + buf_offset, &rand_num, size); 
-    if (err != 0) {
-      return -EFAULT;
+    while(size / 4 > 0) {
+	rand_num = mersenne_random_int();
+	err = copy_to_user(buf + buf_offset, &rand_num, 4);
+	if (err != 0) {
+	    return -EFAULT;
+	}
+	size -= 4;
+	buf_offset += 4;
     }
-  }
-  return 0;
+    // size is in the range 0 to 3
+    if (size != 0) {
+	rand_num = mersenne_random_int();
+	err = copy_to_user(buf + buf_offset, &rand_num, size); 
+	if (err != 0) {
+	    return -EFAULT;
+	}
+    }
+    return 0;
 }
 
 static int mersenne_open(struct inode *node, struct file *file) {
-  // vaugely obscure seed. It really doesn't matter since I don't care if this
-  // module is cryptographically secure.
-  printk(KERN_INFO MODULE_NAME ": Device opened\n");
-  return 0;
+    // vaugely obscure seed. It really doesn't matter since I don't care if this
+    // module is cryptographically secure.
+    printk(KERN_INFO MODULE_NAME ": Device opened\n");
+    return 0;
 }
 
 static int mersenne_close(struct inode *node, struct file *file) {
-  // nothing to do here, just return
-  return 0;
+    // nothing to do here, just return
+    return 0;
 }
 
 static ssize_t mersenne_read(struct file *file, char __user *buf, size_t len, loff_t *off) {
-  // seek is not supported for this char device, so off is completely ignored
-  int err;
-  err = mersenne_fill_buf(buf, len);
-  if (err < 0) {
-    return err;
-  }
-  return len;
+    // seek is not supported for this char device, so off is completely ignored
+    int err;
+    err = mersenne_fill_buf(buf, len);
+    if (err < 0) {
+	return err;
+    }
+    return len;
 }
 
 static const struct file_operations mersenne_file_ops = {
-  .owner    = THIS_MODULE,
-  .open     = mersenne_open,
-  .release  = mersenne_close,
-  .read     = mersenne_read,
-  .llseek   = noop_llseek, // not supported
+    .owner    = THIS_MODULE,
+    .open     = mersenne_open,
+    .release  = mersenne_close,
+    .read     = mersenne_read,
+    .llseek   = noop_llseek, // not supported
 };
 
 static int __init mersenne_module_init(void) {
-  int err;
-  printk(KERN_INFO MODULE_NAME ": Module init\n");
-  // first, allocate a region of character device numbers, including a major number
-  if ((err = alloc_chrdev_region(&device_number, 0, 1, MODULE_NAME)) < 0) {
-    printk(KERN_ERR MODULE_NAME ": Chrdev allocation failed\n");
-    return err;
-  }
-  // then, create a class of character devices owned by this module
-  if ((device_class = class_create(THIS_MODULE, MODULE_NAME)) == NULL) {
-    printk(KERN_ERR MODULE_NAME ": Device class creation failed\n");
-    unregister_chrdev_region(device_number, 1);
-    return -ENOMEM;
-  }
-  // then, create the char device using the allocated major number and device class
-  if (device_create(device_class, NULL, device_number, NULL, MERSENNE_DEVICE_NAME) == NULL) {
-    printk(KERN_ERR MODULE_NAME ": Device create failed\n");
-    unregister_chrdev_region(device_number, 1);
-    class_destroy(device_class);
-    return -ENOMEM;
-  }
-  // once the device is created, initialize it and associate it with the device major number
-  cdev_init(&char_device, &mersenne_file_ops);
-  if ((err = cdev_add(&char_device, device_number, 1)) < 0) {
-    printk(KERN_ERR MODULE_NAME ": cdev_add failed\n");
-    device_destroy(device_class, device_number);
-    class_destroy(device_class);
-    unregister_chrdev_region(device_number, 1);
-    return err;
-  }
-  mersenne_init(jiffies); // seed the mersenne twister using jiffies
-  printk(KERN_INFO MODULE_NAME ": Init success\n");
-  return 0;
+    int err;
+    printk(KERN_INFO MODULE_NAME ": Module init\n");
+    // first, allocate a region of character device numbers, including a major number
+    if ((err = alloc_chrdev_region(&device_number, 0, 1, MODULE_NAME)) < 0) {
+	printk(KERN_ERR MODULE_NAME ": Chrdev allocation failed\n");
+	return err;
+    }
+    // then, create a class of character devices owned by this module
+    if ((device_class = class_create(THIS_MODULE, MODULE_NAME)) == NULL) {
+	printk(KERN_ERR MODULE_NAME ": Device class creation failed\n");
+	unregister_chrdev_region(device_number, 1);
+	return -ENOMEM;
+    }
+    // then, create the char device using the allocated major number and device class
+    if (device_create(device_class, NULL, device_number, NULL, MERSENNE_DEVICE_NAME) == NULL) {
+	printk(KERN_ERR MODULE_NAME ": Device create failed\n");
+	unregister_chrdev_region(device_number, 1);
+	class_destroy(device_class);
+	return -ENOMEM;
+    }
+    // once the device is created, initialize it and associate it with the device major number
+    cdev_init(&char_device, &mersenne_file_ops);
+    if ((err = cdev_add(&char_device, device_number, 1)) < 0) {
+	printk(KERN_ERR MODULE_NAME ": cdev_add failed\n");
+	device_destroy(device_class, device_number);
+	class_destroy(device_class);
+	unregister_chrdev_region(device_number, 1);
+	return err;
+    }
+    mersenne_init(jiffies); // seed the mersenne twister using jiffies
+    printk(KERN_INFO MODULE_NAME ": Init success\n");
+    return 0;
 }
 
 static void __exit mersenne_module_exit(void) {
-  // clean up all of the things that were allocated in the module init function
-  cdev_del(&char_device);
-  device_destroy(device_class, device_number);
-  class_destroy(device_class);
-  unregister_chrdev_region(device_number, 1);
-  printk(KERN_INFO MODULE_NAME ": Module unloading\n");
+    // clean up all of the things that were allocated in the module init function
+    cdev_del(&char_device);
+    device_destroy(device_class, device_number);
+    class_destroy(device_class);
+    unregister_chrdev_region(device_number, 1);
+    printk(KERN_INFO MODULE_NAME ": Module unloading\n");
 }
 
 module_init(mersenne_module_init);
